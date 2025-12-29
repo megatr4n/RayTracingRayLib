d706592209bc23ed21ac7bf2d53d399981d97b88
tajmaha1 <d.cherniavskyi@ukma.edu.ua>
2025-12-29 16:05:26 +0200

    implement texture system with CheckerTexture and refactor Lambertian material to support UV mapping

--- TheNextWeek/include/material.h
+++ TheNextWeek/include/material.h
@@ -1,51 +1,52 @@
 #ifndef RT_MATERIAL_H
 #define RT_MATERIAL_H
 
-#include "ray.h"
-#include "vec3.h"
-#include <cmath>
-
-struct HitRecord;
+#include "rtweekend.h"
+#include "hittable.h"
+#include "texture.h" 
 
 class RTMaterial {
 public:
-    virtual bool scatter(const RTRay& r_in, const HitRecord& rec, Color3& attenuation, RTRay& scattered) const = 0;
     virtual ~RTMaterial() = default;
+    virtual bool scatter(
+        const RTRay& r_in, const HitRecord& rec, Color3& attenuation, RTRay& scattered
+    ) const = 0;
 };
 
 class Lambertian : public RTMaterial {
 public:
-    Color3 albedo;
+    Lambertian(const Color3& albedo);
+    Lambertian(std::shared_ptr<Texture> tex);
 
-    Lambertian(const Color3& a) : albedo(a) {}
+    bool scatter(const RTRay& r_in, const HitRecord& rec, Color3& attenuation, RTRay& scattered) const override;
 
-    virtual bool scatter(const RTRay& r_in, const HitRecord& rec, Color3& attenuation, RTRay& scattered) const override;
+private:
+    std::shared_ptr<Texture> tex; 
 };
 
 class Metal : public RTMaterial {
 public:
+    Metal(const Color3& albedo, double fuzz);
+    bool scatter(const RTRay& r_in, const HitRecord& rec, Color3& attenuation, RTRay& scattered) const override;
+
+private:
     Color3 albedo;
     double fuzz;
-
-    Metal(const Color3& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
-
-    virtual bool scatter(const RTRay& r_in, const HitRecord& rec, Color3& attenuation, RTRay& scattered) const override;
 };
 
 class Dielectric : public RTMaterial {
 public:
-    double ir;
-
-    Dielectric(double index_of_refraction) : ir(index_of_refraction) {}
-
-    virtual bool scatter(const RTRay& r_in, const HitRecord& rec, Color3& attenuation, RTRay& scattered) const override;
+    Dielectric(double refraction_index);
+    bool scatter(const RTRay& r_in, const HitRecord& rec, Color3& attenuation, RTRay& scattered) const override;
 
 private:
+    double refraction_index;
+
     static double reflectance(double cosine, double ref_idx) {
-        double r0 = (1 - ref_idx) / (1 + ref_idx);
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
         r0 = r0 * r0;
-        return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
     }
 };
 
-#endif 
+#endif
--- TheNextWeek/include/texture.h
+++ TheNextWeek/include/texture.h
@@ -1,97 +1,52 @@
-#ifndef TEXTURE_H
-#define TEXTURE_H
+#ifndef RT_TEXTURE_H
+#define RT_TEXTURE_H
 
+#include "rtweekend.h"
+#include "vec3.h"
 
-#include "perlin.h"
-#include "rtw_stb_image.h"
-
-
-
-
-class texture {
-  public:
-    virtual ~texture() = default;
-    virtual color value(double u, double v, const point3& p) const = 0;
+class Texture {
+public:
+    virtual ~Texture() = default;
+    virtual Color3 value(double u, double v, const Point3& p) const = 0;
 };
 
+class SolidColor : public Texture {
+public:
+    SolidColor(const Color3& albedo) : albedo(albedo) {}
+    SolidColor(double red, double green, double blue) : SolidColor(Color3(red, green, blue)) {}
 
-class solid_color : public texture {
-  public:
-    solid_color(const color& albedo) : albedo(albedo) {}
-
-    solid_color(double red, double green, double blue) : solid_color(color(red,green,blue)) {}
-
-    color value(double u, double v, const point3& p) const override {
+    Color3 value(double u, double v, const Point3& p) const override {
         return albedo;
     }
 
-  private:
-
-    color albedo;
-
+private:
+    Color3 albedo;
 };
 
+class CheckerTexture : public Texture {
+public:
+    CheckerTexture(double scale, std::shared_ptr<Texture> even, std::shared_ptr<Texture> odd)
+        : inv_scale(1.0 / scale), even(even), odd(odd) {}
 
-class checker_texture : public texture {
-  public:
-    checker_texture(double scale, shared_ptr<texture> even, shared_ptr<texture> odd) : inv_scale(1.0 / scale), even(even), odd(odd) {}
+    CheckerTexture(double scale, const Color3& c1, const Color3& c2)
+        : inv_scale(1.0 / scale),
+          even(std::make_shared<SolidColor>(c1)),
+          odd(std::make_shared<SolidColor>(c2)) {}
 
-    checker_texture(double scale, const color& c1, const color& c2) : checker_texture(scale, make_shared<solid_color>(c1), make_shared<solid_color>(c2)) {}
-
-    color value(double u, double v, const point3& p) const override {
-        auto xInteger = int(std::floor(inv_scale * p.x()));
-        auto yInteger = int(std::floor(inv_scale * p.y()));
-        auto zInteger = int(std::floor(inv_scale * p.z()));
+    Color3 value(double u, double v, const Point3& p) const override {
+        auto xInteger = static_cast<int>(std::floor(inv_scale * p.x));
+        auto yInteger = static_cast<int>(std::floor(inv_scale * p.y));
+        auto zInteger = static_cast<int>(std::floor(inv_scale * p.z));
 
         bool isEven = (xInteger + yInteger + zInteger) % 2 == 0;
 
         return isEven ? even->value(u, v, p) : odd->value(u, v, p);
     }
 
-  private:
-
+private:
     double inv_scale;
-    shared_ptr<texture> even;
-    shared_ptr<texture> odd;
-    
+    std::shared_ptr<Texture> even;
+    std::shared_ptr<Texture> odd;
 };
-
-
-class image_texture : public texture {
-  public:
-    image_texture(const char* filename) : image(filename) {}
-
-    color value(double u, double v, const point3& p) const override {
-        if (image.height() <= 0) return color(0,1,1);
-
-        u = interval(0,1).clamp(u);
-        v = 1.0 - interval(0,1).clamp(v);  
-
-        auto i = int(u * image.width());
-        auto j = int(v * image.height());
-        auto pixel = image.pixel_data(i,j);
-
-        auto color_scale = 1.0 / 255.0;
-        return color(color_scale*pixel[0], color_scale*pixel[1], color_scale*pixel[2]);
-    }
-
-  private:
-    rtw_image image;
-};
-
-
-class noise_texture : public texture {
-  public:
-    noise_texture(double scale) : scale(scale) {}
-
-    color value(double u, double v, const point3& p) const override {
-        return color(.5, .5, .5) * (1 + std::sin(scale * p.z() + 10 * noise.turb(p, 7)));
-    }
-
-  private:
-    perlin noise;
-    double scale;
-};
-
 
 #endif
